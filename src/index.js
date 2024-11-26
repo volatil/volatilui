19c0a56d4f727da245affe9001d1adf9ede0d11a
Paulo Torrealba <contacto@volatil.cl>
2024-11-26 11:32:15 -0300

    Input y Svg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Saludar from "./lib/Saludar";
 import Button from "./lib/Button";
 import Textarea from "./lib/Textarea";
+import Input from "./lib/Input";
 
-export { Saludar, Button, Textarea };
+export { Saludar, Button, Textarea, Input };
---
+++ src/lib/Input.js
@@ -0,0 +1,33 @@
+import "../css/labels.css"
+import Button from "./Button";
+import Svg from "../components/Svg";
+
+export default function Input({ onChange = () => {}, style={}, className="", label, value, type = "text", accept="image/png, image/jpeg", disabled, nombrearchivo }) {
+    return (
+        type === "file"
+        ? (
+            <Button className={`typefile ${ className }`} style={{ position: "relative" }}>
+                <input onChange={ onChange } type="file" accept={ accept } />
+                <Svg style={{ height: "15px" }} icono="upload" color="var( --gris )" />
+                <p>{ nombrearchivo ? nombrearchivo : "Cargar imagen" }</p>
+            </Button>
+        )
+        : (
+            <div
+                style={ style }
+                data-type={ type }
+                className={`elcampo ${ className }`}
+            >
+                <input
+                    data-pista={ label }
+                    type={ type }
+                    defaultValue={ value }
+                    disabled={ disabled }
+                    required
+                />
+                { type === "file" && <Svg style={{ width: "40px" }} icono="upload" color="var( --gris )" /> }
+                <label htmlFor={ label }> { label } </label>
+            </div>
+        )
+    )
+}
